14c80aa18681a05ac2168e10773074bb1f1fbbbe
Zmhovo <1060037850@qq.com>
2024-10-13 17:00:44 +0800

    Final commit

---
+++ build/CMakeFiles/leptjson.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson.dir/src/leptjson.c.o"
+  "CMakeFiles/leptjson.dir/src/leptjson.c.o.d"
+  "CMakeFiles/leptjson.dir/src/test.c.o"
+  "CMakeFiles/leptjson.dir/src/test.c.o.d"
+  "leptjson"
+  "leptjson.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leptjson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
